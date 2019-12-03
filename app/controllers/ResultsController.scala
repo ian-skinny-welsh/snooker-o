57783cfdd21aabb94ab19a138c55b0529f1a5382
Ian Welsh <ian.welsh@atos.net>
2019-12-03 00:03:45 +0000

    Updated the export facility, few other minor tweaks to clean up text.

--- app/controllers/ResultsController.scala
+++ app/controllers/ResultsController.scala
@@ -57,40 +57,43 @@ class ResultsController @Inject() (cc:MessagesControllerComponents)
 
     val conf = ConfigFactory.load
     val basePath = conf.getString("export.basePath")
-    val baseDir = new File(basePath)
-    if(!baseDir.exists()) baseDir.mkdir()
+    createDirectories(basePath)
 
     val summaryResultsPath = basePath + SUMMARY_RESULTS_NAME
-
-    val summaryResultsPage = views.html.results.core.results_wrapper(SUMMARY_RESULTS_TITLE, views.html.results.core.summary_results_core(CompetitorProcessor.getCurrentDataSet, true))
-
-    using(new BufferedWriter(new FileWriter(new File(summaryResultsPath), false))) { outputFile =>
-        outputFile.write(summaryResultsPage.toString + "\n")
-    }
-
-    val newDir = new File(basePath, DETAILED_RESULTS_PATH)
-    if(!newDir.exists()) newDir.mkdir()
-
-    logger.warn(s"Does directory exist: ${newDir.exists()}")
+    val summaryResultsPage = views.html.results.core.results_wrapper(SUMMARY_RESULTS_TITLE,
+      views.html.results.core.summary_results_core(CompetitorProcessor.getCurrentDataSet, true))
+    outputToFile(summaryResultsPath, summaryResultsPage.toString)
 
     val data = CompetitorProcessor.getCurrentDataSet
     for(
       courseClassType <- data.keySet;
-      (placedCompetitor, place) <- data(courseClassType).zip(LazyList from 1)
+      numEntries = data(courseClassType).size;
+      placedCompetitor  <- data(courseClassType)
     ) yield {
-
-      val detaledResultsPage = views.html.results.core.results_wrapper("Detailed results", views.html.results.core.detailed_results_core(courseClassType, placedCompetitor))
-      val path = newDir.getPath + s"\\${courseClassType.toString.toLowerCase}-${place}.html"
-
-      using(new BufferedWriter(new FileWriter(new File(path), false))) { outputFile =>
-        outputFile.write(detaledResultsPage.toString + "\n")
-      }
+      val detailedResultsPage = views.html.results.core.results_wrapper("Detailed results",
+        views.html.results.core.detailed_results_core(courseClassType, placedCompetitor, true, numEntries))
+      val path = basePath + s"${DETAILED_RESULTS_PATH}\\${courseClassType.toString.toLowerCase}-${placedCompetitor.place}.html"
+      outputToFile(path, detailedResultsPage.toString)
     }
 
     Redirect(routes.ResultsController.summaryResultsDisplay)
   }
+
+  private def outputToFile(path: String, content: String) = {
+    using(new BufferedWriter(new FileWriter(new File(path), false))) { outputFile =>
+      outputFile.write(content)
+    }
+  }
+
+  private def createDirectories(basePath: String) = {
+    val baseDir = new File(basePath)
+    if(!baseDir.exists()) baseDir.mkdir()
+
+    val subDir = new File(basePath, DETAILED_RESULTS_PATH)
+    if(!subDir.exists()) subDir.mkdir()
+
+    logger.warn(s"Does directory exist: ${subDir.exists()}")
+  }
 }
 
-// TODO: Fix the "Homepage" button in the detailed results EXPORT.  It needs to go back to the exported summary.
-// TODO: In the exported detailed results, add a "Next" button that goes to the next competitor
 // TODO: Refactor RaceTime - too many classes in one file.
